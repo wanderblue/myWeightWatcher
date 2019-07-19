aa02cc8b8c122c1ede215ab3728edfc5f4558958
wanderblue <wanderblue691@gmail.com>
2019-07-18 21:17:15 -0700

    mongo connection

--- database/index.js
+++ database/index.js
@@ -4,21 +4,28 @@ mongoose.Promise = global.Promise
 
 //your local database url
 //27017 is the default mongoDB port
-const uri = 'mongodb://localhost:27017/user-passport' 
+//const uri = 'mongodb://localhost:27017/user-passport' 
 
-mongoose.connect(uri).then(
-    () => { 
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/user-passport"
+);
+
+//const uri = 'mongodb://localhost:27017/user-passport' 
+
+//mongoose.connect(uri).then(
+  //  () => { 
         /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ 
-        console.log('Connected to Mongo');
+    //    console.log('Connected to Mongo');
         
-    },
-    err => {
+    //},
+    //err => {
          /** handle initial connection error */ 
-         console.log('error connecting to Mongo: ')
-         console.log(err);
+      //   console.log('error connecting to Mongo: ')
+        // console.log(err);
          
-        }
-  );
+        //}
+  //);
 
 
 module.exports = mongoose.connection
